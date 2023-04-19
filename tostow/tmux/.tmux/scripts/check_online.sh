ccd59af307f0db6f50aa0ba19540c3f1fd377d54
boticelli <kandelakitina@gmail.com>
2023-04-19 13:33:48 +0300

    Add check online script to TMUX pane

---
+++ tostow/tmux/.tmux/scripts/check_online.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if the computer is online by pinging a reliable host
+ping -c 1 -W 1 8.8.8.8 > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "#[fg=green]Online"
+else
+    echo "#[fg=red]Offline"
+fi
