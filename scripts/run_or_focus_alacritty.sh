652430d407578398353c049b9adf1ab2e5079185
boticelli <kandelakitina@gmail.com>
2023-07-25 19:55:10 +0300

    Make alacritty always run in the current workspace

--- scripts/run_or_focus_alacritty.sh
+++ scripts/run_or_focus_alacritty.sh
@@ -5,8 +5,14 @@ if pgrep -x "alacritty" > /dev/null; then
   # Get the window ID of the first Alacritty instance
   ALACRITTY_WID=$(xdotool search --onlyvisible --class alacritty | head -1)
   
+  # Get the current workspace ID
+  CURRENT_WORKSPACE=$(xdotool get_desktop)
+  
   # Activate the Alacritty window if a valid window ID is found
   if [ -n "$ALACRITTY_WID" ]; then
+    # Move Alacritty to the current workspace
+    xdotool set_desktop_for_window "$ALACRITTY_WID" "$CURRENT_WORKSPACE"
+    # Activate the Alacritty window
     xdotool windowactivate "$ALACRITTY_WID"
   else
     nixGLIntel alacritty &
